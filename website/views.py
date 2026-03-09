1a73933b9a3ca939ee75c7e7838ab1ae24247bc1
Bhavana <bhavana.career.xx@gmail.com>
2026-03-09 15:22:16 +0530

    Initial commit - Django layout project

---
+++ website/views.py
@@ -0,0 +1,11 @@
+#Render function
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'website/home.html')
+
+def about(request):
+    return render(request,'website/about.html')
+
+def contact(request):
+    return render(request,'website/contact.html')
